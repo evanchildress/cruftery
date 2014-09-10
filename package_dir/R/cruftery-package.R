738486c3ed42d4fc3df69c5ff7f57a2b6829e1fa
Nicholas G Reich <nick@schoolph.umass.edu>
2014-09-10 15:11:06 -0400

    adding province data.

---
+++ package_dir/R/cruftery-package.R
@@ -0,0 +1,13 @@
+##' @name thai_prov_data
+##' @title Data on provinces in Thailand
+##' @description A data frame with information about Thai provinces, including different identifiers, an English language name, region, area, and population (based on the 2010 census). 
+##' @docType data
+##' @format A data frame. 
+##' 
+##' @usage data(thai_prov_data)
+##' @source Accessed: 10 April 2014 \url{http://www.statoids.com/uth.html}
+##' 
+##' @examples
+##' data(thai_prov_data)
+##' @keywords datasets
+NULL
