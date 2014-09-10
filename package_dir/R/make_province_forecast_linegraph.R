e947930a197af135834232c85468f2a5ef8395c1
Nicholas G Reich <nick@schoolph.umass.edu>
2014-09-10 12:49:05 -0400

    better time scale in linegraph figure, adding province linegraphs.

--- package_dir/R/make_province_forecast_linegraph.R
+++ package_dir/R/make_province_forecast_linegraph.R
@@ -23,21 +23,21 @@ make_country_prediction_line_graph <- function(forecasts, counts, ylim_scale=1) 
                                date_sick = biweek_to_date(biweek, year))
                 forecast_times <- (forecasts_cntry$year + (forecasts_cntry$biweek-1)/26)
                 
-                counts_cntry <- counts %>% 
-                        group_by(date_sick_year, date_sick_biweek) %>%
+                counts_prov <- counts %>% 
+                        group_by(date_sick_year, date_sick_biweek, province) %>%
                         summarize(cntry_count = sum(count)) %>%
                         mutate(time = date_sick_year + (date_sick_biweek-1)/26,
                                date_sick = biweek_to_date(date_sick_biweek, date_sick_year),
                                forecast_biweek = time %in% forecast_times)
 
-                ## add column in counts_cntry indicating which biweeks were left out of the fit
-                
+                ## add column in counts_prov indicating which biweeks were left out of the fit
+                                
                 ## make plot
                 ggplot() + theme_bw() + 
                         theme(legend.position=c(1,1), legend.justification=c(1,1),
                               axis.text.x = element_text(angle = 90, hjust = 1, vjust=.5)) +
                         ## plot counts
-                        geom_bar(data=counts_cntry, 
+                        geom_bar(data=counts_prov, 
                                  aes(x=date_sick, y=cntry_count, fill=forecast_biweek), 
                                  stat="identity") + 
                         scale_fill_manual(values=c("black", "gray"),
@@ -49,11 +49,11 @@ make_country_prediction_line_graph <- function(forecasts, counts, ylim_scale=1) 
                         geom_ribbon(data=forecasts_cntry, aes(x=date_sick, 
                                                               ymin=predicted_lb, ymax=predicted_ub), 
                                                               alpha=I(.1)) +
+                        facet_wrap(~province, ncol=)
                         # air-brushing
                         scale_x_date(breaks = "1 month",
                                      labels = date_format("%d %b %Y"),
                                      limits = as.Date(c('2013-01-01','2015-01-01')))+
                         xlab(NULL) + ylab(NULL) + 
-                        ylim(0, max(counts_cntry$cntry_count)*ylim_scale) +
                         ggtitle("Observed and predicted DHF case counts for all of Thailand")
         }
