05f8198d563c6ec4d1ba5c45fc98e527db64bf0b
Nicholas G Reich <nick@schoolph.umass.edu>
2014-09-11 15:22:09 -0400

    add region to province linegraphs

--- package_dir/R/make_province_forecast_linegraph.R
+++ package_dir/R/make_province_forecast_linegraph.R
@@ -6,7 +6,7 @@
 #'@param counts_file file containing counts
 #'
 
-make_province_prediction_line_graph <- function(forecasts, counts) {
+make_province_prediction_line_graph <- function(forecasts, counts, region=1) {
                 require(dplyr)
                 require(lubridate)
                 require(cruftery)
@@ -18,7 +18,7 @@ make_province_prediction_line_graph <- function(forecasts, counts) {
                 counts <- tbl_df(counts)
                         
                 ## date plotting limits
-                min_plot_date <- as.Date('2013-01-01')
+                min_plot_date <- as.Date('2012-01-01')
                 
                 ## aggregate to country-level
                 forecasts_prov <- forecasts %>% group_by(biweek, year, pid, pname) %>% 
@@ -27,6 +27,9 @@ make_province_prediction_line_graph <- function(forecasts, counts) {
                                   predicted_lb = sum(lb)) %>%
                         mutate(time = year + (biweek-1)/26,
                                date_sick = biweek_to_date(biweek, year))
+                forecasts_prov <- left_join(forecasts_prov, thai_prov_data)
+                forecasts_prov <- filter(forecasts_prov, MOPH_Admin_Code == region)
+                
                 forecast_times <- (forecasts_prov$year + (forecasts_prov$biweek-1)/26)
                 
                 counts_prov <- counts %>% 
@@ -43,30 +46,33 @@ make_province_prediction_line_graph <- function(forecasts, counts) {
                                          pid=FIPS,
                                          pname=Province)
                 counts_prov <- left_join(counts_prov, thai_prov_data)
+                counts_prov <- filter(counts_prov,MOPH_Admin_Code == region)
+                        
                                             
                 ## add column in counts_prov indicating which biweeks were left out of the fit
                                 
                 ## make plot
+                plot_title <- paste("Observed and predicted DHF case counts for MOPH Region", region)
                 ggplot() + theme_bw() + 
-                        theme(#legend.position=c(1,1), legend.justification=c(1,1),
+                        theme(legend.position="bottom", #legend.justification=c(1,1),
                               axis.text.x = element_text(angle = 90, hjust = 1, vjust=.5)) +
                         ## plot counts
-                        geom_bar(data=filter(counts_prov, date_sick >= as.Date('2013-01-01')), 
+                        geom_bar(data=counts_prov, 
                                  aes(x=date_sick, y=prov_count, fill=forecast_biweek), 
                                  stat="identity") + 
                         scale_fill_manual(values=c("black", "gray"),
-                                          name="Forecast data",
-                                          labels=c("used", "unused"))+
+                                          name=NULL,
+                                          labels=c("used by forecast model", "not used by forecast model"))+
                         ## add forecasts
                         geom_line(data=forecasts_prov, aes(x=date_sick, y=predicted_prov_count)) +
                         geom_point(data=forecasts_prov, aes(x=date_sick, y=predicted_prov_count)) +
                         geom_ribbon(data=forecasts_prov, aes(x=date_sick, 
                                                               ymin=predicted_lb, ymax=predicted_ub), 
                                                               alpha=I(.1)) +
-                        facet_wrap(MOPH_Admin_Code~pname, scales="free_y") +
+                        facet_grid(pname~., scales="free_y") +
                         # air-brushing
                         scale_x_date(breaks = "3 months",
-                                     labels = date_format("%b '%y"))+
+                                     labels = date_format("%d %b %Y"))+
                         xlab(NULL) + ylab(NULL) + #ylim(0, 1000) +
-                        ggtitle("Observed and predicted DHF case counts for all of Thailand")
+                        ggtitle(plot_title)
         }
